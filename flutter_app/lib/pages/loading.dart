ce7ce60c549bdf108f34cb886a6b72f263bc6a59
Sithija Siriwardana <sithijalawan@gmail.com>
2020-03-05 12:05:56 +0530

    services added

--- flutter_app/lib/pages/loading.dart
+++ flutter_app/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:flutterapp/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,26 +8,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
+  String time = 'loading';
 
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-    // print(response.body);
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
