ef1d81d1fedea523b0bef9c2e206ce852a531b71
Sithija Siriwardana <sithijalawan@gmail.com>
2020-03-06 21:47:30 +0530

    countries added

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'pages/home.dart';
-import 'pages/loading.dart';
-import 'pages/choose_location.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/' : (context) => Loading(),
-    '/home' : (context) => Home(),
-    '/location' : (context) => ChooseLocation(),
-  },
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    }
 ));
 
